59844a510d949041195bedb836d1b917a52d8c3e
Francotte <yohanfrancotte@hotmail.com>
2015-10-16 14:35:37 +0200

    Ajour fonction Bizz

---
+++ src/exoGit/FonctionBizz.java
@@ -0,0 +1,13 @@
+package exoGit;
+
+public class FonctionBizz {
+	public int n;
+	public static void main(String[] args) 
+	{
+	bizz(10);
+	}
+	public static void bizz(int n) 
+	{
+		System.out.println(n);
+	}
+}
