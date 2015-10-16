7de96b08c5d8f12e4c2ff3f381cef1be51d4381b
Francotte <yohanfrancotte@hotmail.com>
2015-10-16 14:48:15 +0200

    Ajour fonction Buzz

--- src/exoGit/FonctionBizz.java
+++ src/exoGit/FonctionBizz.java
@@ -1,13 +1,18 @@
 package exoGit;
 
 public class FonctionBizz {
-	public int n;
+	public int n,m;
 	public static void main(String[] args) 
 	{
-	bizz(10);
+	bizz(9);
+	buzz(10);
 	}
 	public static void bizz(int n) 
 	{
 		System.out.println(n);
+	}
+	public static void buzz(int m);
+	{
+		System.out.println(m);
 	}
 }
